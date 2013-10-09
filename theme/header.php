2468c2b9b641e68dfbcc66d4d3ff44d9daf779f6
weiyih <cheong@intalio.com>
2013-10-09 17:19:21 +0800

    Some fine tuning.

--- theme/header.php
+++ theme/header.php
@@ -22,7 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-  <div id="page" class="hfeed site">
+  <div id="page" class="hfeed site <?php echo is_front_page()?'home-page':'other-page'; ?>">
     <header id="masthead" class="site-header" role="banner">
       <a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
         <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
